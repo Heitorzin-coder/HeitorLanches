794fa2923c9b3f917e8a3301f5e79486753c256b
Heitor-Pampinha-dos-codigo <renattobarretto4321@gmail.com>
2026-03-26 21:03:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,56 @@
 // =====================================
 // CONFIGURAÇÕES
 // =====================================
-const emailVendas = "gglimportes@gmail.com"; // troque se quiser
+const emailVendas = "renattobarretto4321@gmail.com"; // troque se quiser
 
 // =====================================
 // PRODUTOS (VOCÊ EDITA AQUI PELO CELULAR)
 // =====================================
 const produtos = [
   {
-    nome: "Cartela de 55 adesivos para bicicleta (Foto Meramente Ilustrativa)",
-    preco: 20,
-    imagem: "55 adesivos.jpeg"
-  },
-  {
-    nome: "Boné de marca",
-    preco: 90,
-    imagem: "boné de marca.jpeg"
-  },
-  {
-    nome: "Punho de bicicleta",
-    preco: 20,
-    imagem: "punho de bicicleta.jpeg"
-  },
-  {
-    nome: "Banco de bicicleta",
-    preco: 30,
-    imagem: "banco de bicicleta.jpeg"
-  },
-  {
-    nome: "Relógio smartwatch",
-    preco: 100,
-    imagem: "relogio smartwatch.jpeg"
-  },
-  {
-    nome: "16 adesivos (Foto Meramente Ilustrativa)",
-    preco: 16,
-    imagem: "16 adesivos.jpeg"
-  },
-  {
-    nome: "Chuteira Nike Original Society (Foto Meramente Ilustrativa)",
-    preco: 290,
-    imagem: "chuteira nike original society.jpeg"
-  },
-  {
-    nome: "Patinho de guidão",
-    preco: 10,
-    imagem: "patinho de guidão.jpeg"
-  },
-  {
-    nome: "Protetor de Quadro",
-    preco: 14.99,
-    imagem: "protetor de quadro.jpeg"
-  },
-  {
-    nome: "Kit de Cordão",
+    nome: "Hamburguer Normal (Foto Meramente Ilustrativa)",
     preco: 15,
-    imagem: "kit cordão.jpeg"
+    imagem: ""
   },
   {
-    nome: "Pezinho de Bicicleta",
+    nome: "Hamburguer Grande (Foto Meramente Ilustrativa)",
+    preco: 20,
+    imagem: ""
+  },
+  {
+    nome: "Pizza Pequena (Foto Meramente Ilustrativa)",
+    preco: 15,
+    imagem: ""
+  },
+  {
+    nome: "Pizza Grande (Foto Meramente Ilustrativa)",
+    preco: 35,
+    imagem: ""
+  },
+  {
+    nome: "Coxa de Frango (Foto Meramente Ilustrativa)",
     preco: 25,
-    imagem: "pézinho de bike.jpeg"
+    imagem: ""
   },
   {
-    nome: "Banco Selim de Grau para Bicicleta",
-    preco: 60,
-    imagem: "banco selim grau para bicicleta.jpeg"
+    nome: "Coca-Cola Gelada (Foto Meramente Ilustrativa)",
+    preco: 5,
+    imagem: ""
   },
   {
-    nome: "Guidom de Bicicleta Profissional",
-    preco: 79.99,
-    imagem: "guidao de bike profissional.jpeg"
+    nome: "Guaraná Gelado (Foto Meramente Ilustrativa)",
+    preco: 5,
+    imagem: ""
   },
   {
-    nome: "Pedal de Ferro",
-    preco: 40,
-    imagem: "pedal de ferro.jpeg"
+    nome: "Petit Gateau (Foto Meramente Ilustrativa)",
+    preco: 10,
+    imagem: ""
+  },
+  {
+    nome: "Pudim (Foto Meramente Ilustrativa)",
+    preco: 10,
+    imagem: ""
   },
 ];
 
